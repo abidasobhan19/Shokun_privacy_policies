c46f8ecfbbea1d03afb629cb65bc2472d0cc7395
abidasobhan19 <abidasobhan19@gmail.com>
2020-10-05 04:01:12 -0700

    Updates

---
+++ precache-manifest.393181b3765e05d2f91b84927eb68607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca1235acd6fa318e856aaaaa6c1f120",
+    "url": "/Shokun_privacy_policies/index.html"
+  },
+  {
+    "revision": "e3c9231c3fd985fd5f8e",
+    "url": "/Shokun_privacy_policies/static/css/main.d805107c.chunk.css"
+  },
+  {
+    "revision": "da3ff26809d7c2efdfaa",
+    "url": "/Shokun_privacy_policies/static/js/2.1191182e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shokun_privacy_policies/static/js/2.1191182e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c9231c3fd985fd5f8e",
+    "url": "/Shokun_privacy_policies/static/js/main.0530bed4.chunk.js"
+  },
+  {
+    "revision": "330f38ddfc52a1505db4",
+    "url": "/Shokun_privacy_policies/static/js/runtime-main.49173a9b.js"
+  }
+]);
