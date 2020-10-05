b1bf3af247ed35d90412d86cb39e25f9b0345152
abidasobhan19 <abidasobhan19@gmail.com>
2020-10-05 03:54:56 -0700

    inital Commit

---
+++ src/component/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ShokunSNPage from "./shokun";
+
+const Main = () => {
+  return (
+    <div>
+      <ShokunSNPage />
+    </div>
+  );
+};
+
+export default Main;
